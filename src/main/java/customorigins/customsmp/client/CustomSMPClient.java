2092806f3e020282b0dea28f54f887df86c3494f
row454 <rowanhope5@gmail.com>
2021-11-25 08:35:50 +0000

    Initial commit.

---
+++ src/main/java/customorigins/customsmp/client/CustomSMPClient.java
@@ -0,0 +1,13 @@
+package customorigins.customsmp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class CustomSMPClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/customorigins/customsmp/common/CustomSMP.java
@@ -0,0 +1,11 @@
+package customorigins.customsmp.common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CustomSMP implements ModInitializer {
+    public static final String MOD_ID = "customsmp";
+    @Override
+    public void onInitialize() {
+
+    }
+}
